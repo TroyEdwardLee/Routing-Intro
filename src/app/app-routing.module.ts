14fa7cbb95df4af40aaf5d9d625f81a35701a5cf
Troy.Edward.Lee <39561789+TroyEdwardLee@users.noreply.github.com>
2018-11-19 23:54:10 +0800

    [FEATURE] Angular 6 Basics 30 - Configuring child routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,23 @@ import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SettingsComponent } from './settings/settings.component';
+import { SettingsProfileComponent } from './settings-profile/settings-profile.component';
+import { SettingsContactComponent } from './settings-contact/settings-contact.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Route[] = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'settings', component: SettingsComponent },
+  {
+    path: 'settings',
+    component: SettingsComponent,
+    children: [
+      { path: '', redirectTo: 'profile', pathMatch: 'full' },
+      { path: 'profile', component: SettingsProfileComponent },
+      { path: 'contact', component: SettingsContactComponent },
+      { path: '**', redirectTo: 'profile', pathMatch: 'full' }
+    ]
+  },
   { path: '**', component: PageNotFoundComponent }
 ];
 
